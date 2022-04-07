c763e16d5222dc9b392a9a1b5b357cb9b2464b98
Tony <58075305+me-tony@users.noreply.github.com>
2022-04-07 20:00:59 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - Hello World number 2
+          Learn React - Hello World number 3
         </a>
       </header>
     </div>
