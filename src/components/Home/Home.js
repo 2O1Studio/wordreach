d6c1109f830aeb93ef77355bdd3cd1ad1dfcaabf
solensa <39489819+solensa@users.noreply.github.com>
2022-04-09 10:26:46 +0100

    changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
+import "./Home.scss";
+import LetterBox from "../LetterBox/LetterBox";
 
 const Home = () => {
-  return <div className="App">Welcome to Word Reach</div>;
+  return (
+    <>
+      <div className="title">Welcome to Word Reach</div>
+      <div className="letterGrid">
+        <LetterBox displayLetter="H" />
+        <LetterBox displayLetter="E" />
+        <LetterBox displayLetter="L" />
+        <LetterBox displayLetter="L" />
+        <LetterBox displayLetter="O" />
+        <LetterBox displayLetter="!" />
+        <LetterBox displayLetter="!" />
+        <LetterBox displayLetter="!" />
+      </div>
+    </>
+  );
 };
 
 export default Home;
---
+++ src/components/LetterBox/LetterBox.js
@@ -0,0 +1,8 @@
+import "./LetterBox.scss";
+import React from "react";
+
+const LetterBox = ({ displayLetter }) => {
+  return <div className="letterBox">{displayLetter}</div>;
+};
+
+export default LetterBox;
