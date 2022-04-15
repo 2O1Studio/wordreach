bb2acbed3a128ffa84324c1806c284872bbf0da2
solensa <39489819+solensa@users.noreply.github.com>
2022-04-15 20:31:29 +0100

    branch push

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,16 @@
 import "./App.scss";
-import React from "react";
+import { React, useState } from "react";
 import { Route, Routes, Link } from "react-router-dom";
 import About from "../About/About";
 import Home from "../Home/Home";
+import { DndContext } from "@dnd-kit/core";
+import { Draggable } from "./Draggable";
+import { Droppable } from "./Droppable";
 
 const App = () => {
+  const [isDropped, setIsDropped] = useState(false);
+  const draggableMarkup = <Draggable>Drag me</Draggable>;
+
   return (
     <div className="App">
       <div className="header">
@@ -20,8 +26,18 @@ const App = () => {
       <div className="footer">
         <Link to="/about">About</Link>
       </div>
+      <DndContext onDragEnd={handleDragEnd}>
+        {!isDropped ? draggableMarkup : null}
+        <Droppable>{isDropped ? draggableMarkup : "Drop here"}</Droppable>
+      </DndContext>
     </div>
   );
+
+  function handleDragEnd(event) {
+    if (event.over && event.over.id === "droppable") {
+      setIsDropped(true);
+    }
+  }
 };
 
 export default App;
