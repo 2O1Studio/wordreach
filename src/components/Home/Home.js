2daaae6e842805f6f3360a78c14c4d880044fab6
Ben Sterling <info@benjaminsterling.co.uk>
2022-04-23 17:51:41 +0100

    Add placement validation and confirm word button
    Co-authored-by: Tony <me-tony@users.noreply.github.com>
    Co-authored-by: solensa <solensa@users.noreply.github.com>

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,45 @@
-import { React, useState, useEffect } from "react";
+import { React, useState } from "react";
 import "./Home.scss";
 import LetterBox from "../LetterBox/LetterBox";
 import { DndContext } from "@dnd-kit/core";
 import { Draggable } from "../Draggable/Draggable";
 
+const validateWord = async (word) => {
+  const res = await fetch(
+    `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+  );
+  const status = await res.status;
+  // const data = await res.json();
+  if (status === 200) {
+    return true;
+  }
+  return false;
+};
+
 const initialPlayableLetters = [
-  { id: "1", letter: "a", played: false },
-  { id: "2", letter: "b", played: false },
-  { id: "3", letter: "c", played: false },
-  { id: "4", letter: "d", played: false },
-  { id: "5", letter: "e", played: false },
-  { id: "6", letter: "f", played: false },
-  { id: "7", letter: "g", played: false },
-  { id: "8", letter: "h", played: false },
+  { id: "1", letter: "n", played: false },
+  { id: "2", letter: "t", played: false },
+  { id: "3", letter: "l", played: false },
+  { id: "4", letter: "e", played: false },
+  { id: "5", letter: "m", played: false },
+  { id: "6", letter: "t", played: false },
+
+  { id: "7", letter: "a", played: false },
+  { id: "8", letter: "t", played: false },
   { id: "9", letter: "i", played: false },
-  { id: "10", letter: "j", played: false },
+  { id: "10", letter: "n", played: false },
+  { id: "11", letter: "n", played: false },
+  { id: "12", letter: "t", played: false },
+  { id: "13", letter: "a", played: false },
+  { id: "14", letter: "h", played: false },
+  { id: "15", letter: "a", played: false },
+  { id: "16", letter: "r", played: false },
+  { id: "17", letter: "h", played: false },
+  { id: "18", letter: "x", played: false },
+  { id: "19", letter: "n", played: false },
+  { id: "20", letter: "p", played: false },
+  { id: "21", letter: "h", played: false },
+  { id: "22", letter: "y", played: false },
 ];
 const initialBoardState = [
   ["", "", "", "", "", "", "", ""],
@@ -28,7 +53,166 @@ const initialBoardState = [
 ];
 
 const LetterTile = ({ id, letter }) => {
-  return <Draggable id={id}>{letter}</Draggable>;
+  return <Draggable id={id}>{letter.toUpperCase()}</Draggable>;
+};
+
+const checkPreviousPosition = (
+  board,
+  currentPosition,
+  axisToDecrement,
+  axis,
+  axisPosition
+) => {
+  let positionToCheck = currentPosition - 1;
+
+  if (positionToCheck < 0) {
+    return currentPosition;
+  }
+
+  const piece = board.find(
+    (i) => i[axisToDecrement] === positionToCheck && i[axis] === axisPosition
+  );
+
+  if (piece.letter !== "") {
+    return checkPreviousPosition(
+      board,
+      positionToCheck,
+      axisToDecrement,
+      axis,
+      axisPosition
+    );
+  }
+  return currentPosition;
+};
+
+const getFirstLetterOfWordOnBoard = (board, arbitraryLetter, axis) => {
+  const oppositeAxis = axis === "row" ? "column" : "row";
+  const startingPosition = arbitraryLetter.played[oppositeAxis];
+  const firstLetterPosition = checkPreviousPosition(
+    board,
+    startingPosition,
+    oppositeAxis,
+    axis,
+    arbitraryLetter.played[axis]
+  );
+
+  const firstFoundLetter = board.find(
+    (b) =>
+      b[axis] === arbitraryLetter.played[axis] &&
+      b[oppositeAxis] === firstLetterPosition
+  );
+  return {
+    letter: firstFoundLetter.letter,
+    [axis]: arbitraryLetter.played[axis],
+    [oppositeAxis]: firstLetterPosition,
+  };
+};
+
+const getWordFromBoard = (board, firstLetter, axis, oppositeAxis) => {
+  let word = "";
+
+  const addLetterToWord = (board, word, currentLetter, axis, oppositeAxis) => {
+    word = word + currentLetter.letter;
+    const nextLetter = board.find(
+      (b) =>
+        b[axis] === currentLetter[axis] &&
+        b[oppositeAxis] === currentLetter[oppositeAxis] + 1
+    );
+    if (nextLetter && nextLetter.letter !== "") {
+      return addLetterToWord(board, word, nextLetter, axis, oppositeAxis);
+    }
+    return word;
+  };
+
+  return addLetterToWord(board, word, firstLetter, axis, oppositeAxis);
+};
+
+const getWordAxis = (board, playedLetters) => {
+  if (playedLetters.length > 1) {
+    return playedLetters[0].played.row === playedLetters[1].played.row
+      ? "row"
+      : "column";
+  }
+
+  // todo: If only 1 letter played, then use the board state to find axis
+  alert("You've only played 1 letter and we don't account for this yet");
+  return "test";
+};
+
+const checkPlayedWordIsValidOnBoard = (board, playableLetters) => {
+  const playedLetters = playableLetters.filter((l) => l.played !== false);
+  if (playedLetters === 0) {
+    return false;
+  }
+  const arbitraryLetter = playedLetters[0];
+
+  if (!arbitraryLetter) {
+    alert("Please play at least 1 letter");
+    return false;
+  }
+
+  const axis = getWordAxis(board, playedLetters);
+  if (
+    !playedLetters.every(
+      (l) => l.played[axis] === playedLetters[0].played[axis]
+    )
+  ) {
+    alert("You can only play a word in one direction at a time");
+    return false;
+  }
+  const oppositeAxis = axis === "row" ? "column" : "row";
+
+  const playedBoard = board.flatMap((row, rowIndex) => {
+    return row.flatMap((letter, columnIndex) => {
+      const foundLetter = playableLetters.find(
+        (l) => l.played.column === columnIndex && l.played.row === rowIndex
+      );
+
+      return {
+        letter: foundLetter ? foundLetter.letter : letter,
+        static: !foundLetter,
+        row: rowIndex,
+        column: columnIndex,
+      };
+    });
+  });
+
+  // This looks backwards to find the first letter
+  const firstLetter = getFirstLetterOfWordOnBoard(
+    playedBoard,
+    arbitraryLetter,
+    axis
+  );
+
+  const boardInAxisDirection = playedBoard.filter((b) => {
+    return b[axis] === firstLetter[axis];
+  });
+  let blankSpaceFound = false;
+  let spaceFoundInWord = false;
+  for (
+    let i = firstLetter[oppositeAxis];
+    i < boardInAxisDirection.length;
+    i++
+  ) {
+    const element = boardInAxisDirection[i];
+    if (spaceFoundInWord) {
+      continue;
+    }
+    if (blankSpaceFound && element.letter !== "") {
+      spaceFoundInWord = true;
+    }
+    if (element.letter === "") {
+      blankSpaceFound = true;
+    }
+  }
+
+  if (spaceFoundInWord) {
+    alert("You have a space in your word.");
+    return false;
+  }
+
+  const word = getWordFromBoard(playedBoard, firstLetter, axis, oppositeAxis);
+  return validateWord(word);
 };
 
 const Home = () => {
@@ -37,9 +221,32 @@ const Home = () => {
     initialPlayableLetters
   );
 
-  const handleDragStart = (event) => {
-    console.log(event);
+  const confirmWord = async () => {
+    const isValid = await checkPlayedWordIsValidOnBoard(board, playableLetters);
+    if (!isValid) {
+      alert("Word not valid");
+      return false;
+    }
+    setBoard((old) => {
+      return old.map((row, rowIndex) => {
+        return row.map((item, columnIndex) => {
+          const playedLetter = playableLetters.find(
+            (o) =>
+              o.played?.row === rowIndex && o.played?.column === columnIndex
+          );
+          if (playedLetter) {
+            return playedLetter.letter;
+          }
+          return item;
+        });
+      });
+    });
+    setPlayableLetters((old) => {
+      return old.filter((letter) => letter.played === false);
+    });
   };
+
+  const handleDragStart = (event) => {};
 
   const handleDragEnd = (event) => {
     setPlayableLetters((oldSet) => {
@@ -124,19 +331,25 @@ const Home = () => {
           })}
         </div>
         <div style={{ display: "flex", flexWrap: "wrap" }}>
-          {playableLetters
-            .filter((i) => i.played === false)
-            .map((letter) => {
-              return (
-                <LetterTile
-                  key={letter.id}
-                  letter={letter.letter}
-                  id={letter.id}
-                />
-              );
-            })}
+          {playableLetters.map((letter, index) => {
+            if (index >= 6) {
+              return null;
+            }
+            if (letter.played !== false) {
+              return null;
+            }
+            return (
+              <LetterTile
+                key={letter.id}
+                letter={letter.letter}
+                id={letter.id}
+              />
+            );
+          })}
         </div>
       </DndContext>
+
+      <button onClick={confirmWord}>confirm word</button>
     </div>
   );
 };
