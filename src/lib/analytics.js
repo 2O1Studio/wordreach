1e11d02442f68233fdfd70edc041c8e429ec0627
Ben Sterling <info@benjaminsterling.co.uk>
2022-05-14 17:20:19 +0100

    fix: use REACT_APP_ prefix for env vars

--- src/lib/analytics.js
+++ src/lib/analytics.js
@@ -1,13 +1,13 @@
 import amplitude from "amplitude-js";
 
 export const logEvent = (event, eventProperties) => {
-  if (process.env.AMPLITUDE_KEY) {
+  if (process.env.REACT_APP_AMPLITUDE_API_KEY) {
     amplitude.getInstance().logEvent(event, eventProperties);
   }
 };
 
 export const initializeAnalytics = () => {
-  if (process.env.AMPLITUDE_KEY) {
-    amplitude.getInstance().init(process.env.AMPLITUDE_KEY);
+  if (process.env.REACT_APP_AMPLITUDE_API_KEY) {
+    amplitude.getInstance().init(process.env.REACT_APP_AMPLITUDE_API_KEY);
   }
 };
