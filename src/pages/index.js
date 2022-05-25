2b5dad16c42ad373dbc90ab5fecc8b97737bec8a
Ben Sterling <25365573+ben-styling@users.noreply.github.com>
2022-05-25 19:17:30 +0100

    fix: app layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,35 +10,38 @@ import Footer from "../components/Footer/Footer";
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="app">
       <div className={styles.header}>
         <Link to="/">
           <img width={236} height={34} src={Logo} alt="Word Reach logo" />
         </Link>
       </div>
 
-      <p
-        style={{
-          textAlign: "center",
-          marginTop: 10,
-          marginBottom: 20,
-          fontWeight: 600,
-          padding: "0 15px",
-          maxWidth: "400px",
-          marginLeft: "auto",
-          marginRight: "auto",
-        }}
-      >
-        To win, you need to cross the board from blue to green by creating words
-        from your given letters!
-      </p>
+      <div className={styles.content}>
+        <p
+          style={{
+            textAlign: "center",
+            marginTop: 10,
+            marginBottom: 20,
+            fontWeight: 600,
+            padding: "0 15px",
+            maxWidth: "400px",
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
+        >
+          To win, you need to cross the board from blue to green by creating
+          words from your given letters!
+        </p>
 
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/challenge/:letterSet" element={<Challenge />} />
-        {/* <Route path="/rules" element={<Rules />} /> */}
-        {/* <Route path="/about" element={<About />} /> */}
-      </Routes>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/challenge/:letterSet" element={<Challenge />} />
+          {/* <Route path="/rules" element={<Rules />} /> */}
+          {/* <Route path="/about" element={<About />} /> */}
+        </Routes>
+      </div>
+
       <Footer />
     </div>
   );
